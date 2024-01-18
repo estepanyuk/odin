98c2c900c565c6ecc8eea6ee97b95512f9ba2b54
ekaterina stepanyuk <eesipenyuk@mail.ru>
2024-01-18 14:58:57 +0700

    delete Developer

--- Developer/modele3/subject_3(OOP)/HEN/HenFactory.py
+++
@@ -1,6 +0,0 @@
-class HenFacrory():
-    from ruHen import RussianHen
-    from molHen import MoldovanHen
-    from blHen import BelarusianHen
-
-HenFacrory()
--- Developer/modele3/subject_4(Sort)/prac_2/1_z/sort.py
+++
@@ -1,6 +0,0 @@
-def sort(student_tuples):
-    return sorted(student_tuples, key=str.lower)
-
-student = ['john', 'A', 15]
-
-print(sort(student))
