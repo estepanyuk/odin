3a119c3248177cc8107fb77660899324f7126638
ekaterina stepanyuk <eesipenyuk@mail.ru>
2024-02-02 12:08:39 +0700

    update file

--- DCB/modele2/subject_6(Practicing problem solving skills)/practice_1.py
+++ DCB/modele2/subject_6(Practicing problem solving skills)/practice_1.py
@@ -1,3 +1,4 @@
+#задача 1
 class Human:
 
     #Статические поля
@@ -16,19 +17,61 @@ class Human:
         print(f'Возраст: {self.age}')
         print(f'Деньги: {self.__money}')
         print(f'Дом: {self.__house}')
+
     @staticmethod
     def default_info():
-        print(Human.default_name,Human.default_age)
-        print(":_)")
+        print(f'default_name: {Human.default_name}')
+        print(f'default_age: {Human.default_age}')
+
+    def __make_deal(self, house, price):
+        self.__money -= price
+        self.__house = house
+
+    def earn_money(self, max_money):
+        self.__money += max_money
+        print(f'На ваш баланс зачислено {max_money} баксов! Ваш баланс:{self.__money}')
+
+    def buy_house(self, house, disc):
+        price = house.final_prace(disc)
+        if self.__money >= price:
+            self.__make_deal(house, price)
+            print("У тебя хватает денег на покупку этого прекрасного сарая")
+        else:
+            print("К сожалению, у тебя не хватит денег ;((( ")
+
+class House:
+
+    def __init__(self, area, price):
+        self._area = area
+        self._price = price
+
+    def final_prace(self, disc):
+        final_prace = self._price * (100 - disc) / 100
+        print(f'Финальная цена: {final_prace}')
+        return final_prace
 
 
-    def __make_deal(self,COSTA,DOMIK):
-        self.__money -= COSTA
-        self.__house = DOMIK
+class SmallHouse(House):
+    default_area = 40
 
-    def earn_money(self,MAYNER):
-        self.__money += MAYNER
-        print('ЧУДО!!!! Мама перевала вам на вкусняшки ',MAYNER,'.руб')
-        print(f'Теперь на вашем бомжатском балансе {self.__money}')
-    #def buy_house(self,house,disceord):
-    #    if self.__money
+    def __init__(self, price):
+        super().__init__(SmallHouse.default_area, price)
+
+
+Human.default_info()
+
+Alex = Human('Alex', 50)
+Alex.info()
+
+Small_House = SmallHouse(100_000)
+Alex.buy_house(Small_House,0)
+
+Alex.earn_money(100_000)
+Alex.buy_house(Small_House, 10)
+
+Alex.info()
+
+
+
+
+
