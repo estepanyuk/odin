05c40a00e7869c5d9ab9edaa428046e25f747fab
ekaterina stepanyuk <eesipenyuk@mail.ru>
2024-02-05 21:55:47 +0700

    update file

--- DCB/modele3/subject 1(working with files, API)/practice_1/task_1.py
+++ DCB/modele3/subject 1(working with files, API)/practice_1/task_1.py
@@ -1,3 +1,41 @@
+import requests
+import json
+
+response = requests.get('https://randomuser.me/api/').json()
+print(type(response))
+
+"""
+#сереализация
+to_python = json.dumps(response, indent=4)
+print(type(to_python))
+print(to_python)
+
+
+#2 вариант
+#десерилизация
+y = json.loads(to_python)
+print(type(y))
+
+print(y["results"][0]["location"]["street"])
+"""
+
+#3 вариант запись в файл
+with open('data.json', 'w') as file:
+    json.dump(response, file)
+
+with open('data.json', 'r') as j:
+    json_data = json.load(j)
+    print(type(json_data))
+    to_python = json.dumps(json_data, indent=4)
+    print(type(to_python))
+    print(to_python)
+
+y = json.loads(to_python)
+print(y["results"][0]["login"])
+
+
+
+
 
 
 
